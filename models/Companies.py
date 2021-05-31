ac0cdeb9f03f83f60ceb9259d5eb5223efcb381a
Mateus Tozoni <mateusjosepretti@gmail.com>
2021-05-30 21:45:49 -0300

    Fix: Fixed methods and removed meta from Companies model

--- models/Companies.py
+++ models/Companies.py
@@ -9,12 +9,11 @@ class Companies(Document):
     created_at = DateTimeField()
     updated_at = DateTimeField(default=datetime.datetime.now)
     deleted_at = DateTimeField(null=True)
-    meta = {'allow_inheritance': True}
 
     def save(self, *args, **kwargs):
         if not self.created_at:
             self.created_at = datetime.datetime.now()
-        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         return super(Companies, self).save(*args, **kwargs)
 
     def delete(self, signal_kwargs=None, **write_concern):
